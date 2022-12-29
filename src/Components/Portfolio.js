f870f82efe11503e4f0edbee0b37b95800172e3d
Pranav Purohit <adi089076@gmail.com>
2022-12-29 22:48:20 +0530

    updated link

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -3,26 +3,26 @@ import { Box, Text } from '@chakra-ui/react'
 import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
 import { Image, Stack, Heading, Divider, ButtonGroup, Button } from '@chakra-ui/react'
 import Tard from './card'
-import { Grid,GridItem } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
 import "./portfolio.css"
 
 function Portfolio() {
     return (
         <Box id="work" bg='#d4f1f9'    >
 
-            <Box display="flex" justifyContent="center"  id="heading_projects" fontWeight="700" gap="12px" margin="80px 0px 60px 0px" >
+            <Box display="flex" justifyContent="center" id="heading_projects" fontWeight="700" gap="12px" margin="80px 0px 60px 0px" >
 
                 <Text>My Creative </Text><Text color="#F40A34">Projects </Text>
 
             </Box>
-            <Box > 
+            <Box >
 
-            <Grid   gap={6} width="80%" margin="auto" className='projects' >
-                <GridItem  ><Tard title="Swiggy Clone" image="https://st1.bgr.in/wp-content/uploads/2022/06/Untitled-design-2022-06-03T124138.156.jpg" /></GridItem>
-                <GridItem  ><Tard title="Shoppers Stop Clone" image="https://sslimages.shoppersstop.com/sys-master/root/h2c/h60/28817986158622/Sponsored-Brands-Main-Banner-Msite2022-1212-NEW-SP-BRANDS-WOMPAGES--WINTER.jpg" /></GridItem>
-                <GridItem  ><Tard title="The Horse Clone" image="https://cdn.dribbble.com/users/968354/screenshots/15379842/media/91515101f296d48f7ee9565b83e1e96e.png?compress=1&resize=400x300&vertical=top" /></GridItem>
-                 
-            </Grid>
+                <Grid gap={6} width="80%" margin="auto" className='projects' >
+                    <GridItem  ><Tard title="The Horse Clone" image="https://cdn.dribbble.com/users/968354/screenshots/15379842/media/91515101f296d48f7ee9565b83e1e96e.png?compress=1&resize=400x300&vertical=top" desc="This React App Project is a Clone of The Horse Website and We have implement Redux in this App for storing Data and I Created Product and Product Description Page with CHAKRA UI where I implemented Sorting and Filtering Functionality and We fetched All Data from Server API which is deployed on Render." tech="REACT JS, REDUX, CHAKRA UI,CSS,JAVASCRIPT " view="https://thehorse.netlify.app/" gitt="https://github.com/mayankCreation0/TheHorse.com.auClone" /></GridItem>
+                    <GridItem  ><Tard title="Shoppers Stop Clone" image="https://sslimages.shoppersstop.com/sys-master/root/h2c/h60/28817986158622/Sponsored-Brands-Main-Banner-Msite2022-1212-NEW-SP-BRANDS-WOMPAGES--WINTER.jpg" desc="This Project is about Building a Ecommerce clothing Web App.We fetched All the Data from Server and I build Product and Admin Page where i Implement sorting and Multiple Filtering Functionality and I used server Api for Sorting and Filtering Functionality." tech="HTML, CSS, JAVASCRIPT, JSON-SERVER" view="https://shoppersstop-1.netlify.app/" gitt="https://github.com/Raghvendra2811/shoppersstop" /></GridItem>
+                    <GridItem  ><Tard title="Swiggy Clone" image="https://st1.bgr.in/wp-content/uploads/2022/06/Untitled-design-2022-06-03T124138.156.jpg" desc="This Project is about Building a Food Web Application.We cloned Almost All Functionality from original Swiggy App.I built Product page and Some Special Card Page where i implement Sorting and Filtering Functionality and Also made basic search bar Functionality in Card Page." tech="HTML, CSS, JAVASCRIPT" view="" gitt="" /></GridItem>
+
+                </Grid>
             </Box>
 
 
--- src/Components/card.js
+++ src/Components/card.js
@@ -1,62 +1,61 @@
 import React from 'react'
-import { Box,Text } from '@chakra-ui/react'
+import { Box, Text } from '@chakra-ui/react'
 import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
-import { Image,Stack,Heading,Divider,ButtonGroup,Button } from '@chakra-ui/react'
-import {FaVideo} from 'react-icons/fa';
-import {BsEyeFill} from "react-icons/bs"
-import {BsGithub} from "react-icons/bs"
+import { Image, Stack, Heading, Divider, ButtonGroup, Button } from '@chakra-ui/react'
+import { FaVideo } from 'react-icons/fa';
+import { BsEyeFill } from "react-icons/bs"
+import { BsGithub } from "react-icons/bs"
+import "./card.css"
 
-function Tard({title,image}) {
+function Tard({ title, image, desc, tech ,view, gitt}) {
   return (
-    <Box  data-aos="zoom-in"  data-aos-delay="50"
-    data-aos-duration="1000"
-    data-aos-easing="ease-in-out"
-    data-aos-mirror="true"
-    data-aos-once="false"
-    data-aos-anchor-placement="top-center">
-           
-           <Card maxW='sm' bg="white">
-  <CardBody>
-    <Image
-      src={image}
-      alt='Green double couch with wooden legs'
-      borderRadius='lg'
-      h="250px"
-    />
-    <Stack mt='6' spacing='3'>
-      <Heading textAlign="center" size='md'>{title}</Heading>
-      <Text>
-        This sofa is perfect for modern tropical spaces, baroque inspired
-        spaces, earthy toned spaces and for people who love a chic design with a
-        sprinkle of vintage design.
-      </Text>
-      <Box display="flex" width="100%">
-      <Text width="40%" color='#F40A34' fontWeight="500" fontSize="15px">
-         Tech Stack:
-      </Text><Text width="100%" color="grey" fontSize="1xl">
-         React, Redux, Chakra Ui, JavaScript, CSS
-      </Text> 
-      </Box>
-      
-       
-    </Stack>
-  </CardBody>
-  <Divider />
-  <CardFooter >
-    <ButtonGroup  spacing='8' width="80%" margin="auto" >
-      <a><Button fontSize="30px" >
-      <FaVideo/>
-      </Button></a>
-      <a > <Button fontSize="30px" >
-        <BsEyeFill/>
-      </Button ></a>
-      <a ><Button fontSize="30px" >
-      <BsGithub/>
-      </Button></a>
-   
-    </ButtonGroup>
-  </CardFooter>
-</Card>
+    <Box data-aos="zoom-in" data-aos-delay="50" 
+      data-aos-duration="1000"
+      data-aos-easing="ease-in-out"
+      data-aos-mirror="true"
+      data-aos-once="false"
+      data-aos-anchor-placement="top-center">
+
+      <Card maxW='sm' bg="white" id="cardd">
+        <CardBody>
+          <Image
+            src={image}
+            alt='Green double couch with wooden legs'
+            borderRadius='lg'
+            h="250px"
+          />
+          <Stack mt='6' spacing='3'>
+            <Heading textAlign="center" size='md'>{title}</Heading>
+            <Text>
+              {desc}
+            </Text>
+            <Box display="flex" width="100%">
+              <Text width="40%" color='#F40A34' fontWeight="500" fontSize="15px">
+                Tech Stack:
+              </Text><Text width="100%" color="grey" fontSize="1xl">
+                {tech}
+              </Text>
+            </Box>
+
+
+          </Stack>
+        </CardBody>
+        <Divider />
+        <CardFooter >
+          <ButtonGroup spacing='8' width="80%" margin="auto" >
+            <a><Button fontSize="30px" >
+              <FaVideo />
+            </Button></a>
+            <a href={view} target="_blank" > <Button fontSize="30px" >
+              <BsEyeFill />
+            </Button ></a>
+            <a href={gitt} target="_blank" ><Button fontSize="30px" >
+              <BsGithub />
+            </Button></a>
+
+          </ButtonGroup>
+        </CardFooter>
+      </Card>
 
 
     </Box>
